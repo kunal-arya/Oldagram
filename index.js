5d5ce2f5efecb60802ad2250161425273b3e2b0f
kunal-arya <85951446+kunal-arya@users.noreply.github.com>
2022-07-14 20:07:30 +0530

    feat: custom scrollbar

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const posts = [
     {
         name: "Vincent van Gogh",
@@ -26,5 +27,66 @@ const posts = [
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
     }
-]
+];
+
+
+const mainEl = document.getElementById("main");
+
+function mainRender(){
+    let main = "";
+    for(let i = 0 ; i < posts.length ; i++){
+
+        main += `                
+        <section class="post">
+            <div class="post-head">
+                <img class="avatar profile-pic" src="" alt="">
+                <div class="head-content">
+                    <p class="bold-text name"></p>
+                    <p class="location"></p>
+                </div>
+            </div>
+            <img class="post-img" src="" alt="Vincent van Gogh Post">
+            <div class="post-icons">
+                <img class="heart" src="./images/icon-heart.png" alt="Heart Icon">
+                <img src="./images/icon-comment.png" alt="Comment Icon">
+                <img src="./images/icon-dm.png" alt="Personal Message Icon">
+            </div>
+            <p class="bold-text likes-number"><span class="likesEl"></span>,492 likes</p>
+            <p class="comment-section"><span class="bold-text comments username"></span> <span class="comment"></span></p>
+            <div class="next-space"></div>
+        </section>`
+    }
+    mainEl.innerHTML = main;
+}
+
+mainRender();
+
+
+const nameEl = document.querySelectorAll(".name");
+const locationEl = document.querySelectorAll(".location");
+const postEl = document.querySelectorAll(".post-img");
+const likesEl = document.querySelectorAll(".likesEl");
+const usernameEl = document.querySelectorAll(".username");
+const commentEl = document.querySelectorAll(".comment");
+const avatarEl = document.querySelectorAll(".avatar");
+
+function renderPost(){
+
+
+    for(let i = 0 ; i <= posts.length ; i++){
+        nameEl[i].textContent = posts[i].name;
+        locationEl[i].textContent = posts[i].location;
+        avatarEl[i].src = posts[i].avatar;
+        postEl[i].src = posts[i].post;
+        likesEl[i].textContent = posts[i].likes;
+        commentEl[i].textContent = posts[i].comment;
+        usernameEl[i].textContent = posts[i].username;
+    }
+};
+
+
+renderPost();
+
+
+
 
